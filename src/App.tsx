24d7092d4ca0ae78e5b552b834788f2cd9ec4329
Certurix <46199479+Certurix@users.noreply.github.com>
2025-01-27 03:21:43 +0100

    Added vertical bars

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import "./App.css";
 import heartbeat from "../public/assets/heartbeat.svg";
 import armor from "../public/assets/shield.svg";
+import hunger from "../public/assets/hamburger.svg";
+import thirst from "../public/assets/bottle.svg";
+import stamina from "../public/assets/run.svg";
 
 function App() {
   return (
     <div className="hud-container">
       <div className="flex flex-col md:flex-row aspect-16/9 px-5 md:px-10 py-5 md:py-10 text-app-text font-inter font-bold">
-        <div className="flex flex-col md:flex-row justify-baseline items-end content-end gap-10 md:gap-30">
+        <div className="flex flex-col md:flex-row justify-between items-end content-end md:gap-15">
           <div className="flex flex-col-reverse">
-            {/* Heartbeat Icon and Health Bar */}
+            {/* Health Bar */}
             <div className="flex flex-col justify-evenly w-full md:w-60">
               <div className="flex items-center">
                 <div className="icon">
-                  <img src={heartbeat} alt="Heartbeat" />
+                  <img src={heartbeat} alt="Health" />
                 </div>
                 <span className="text-xl ml-auto self-end">50%</span>
               </div>
-
               <div className="w-full bg-app-bg rounded-full h-2.5">
                 <div
                   className="bg-app-health h-2.5 rounded-full"
@@ -24,6 +26,7 @@ function App() {
                 ></div>
               </div>
             </div>
+            {/* Armor Bar */}
             <div className="flex flex-col justify-evenly w-full md:w-60">
               <div className="flex items-center">
                 <div className="icon">
@@ -31,7 +34,6 @@ function App() {
                 </div>
                 <span className="text-xl ml-auto self-end">50%</span>
               </div>
-
               <div className="w-full bg-app-bg rounded-full h-2.5">
                 <div
                   className="bg-app-armor h-2.5 rounded-full"
@@ -40,7 +42,42 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="flex flex-col md:flex-row gap-5 md:gap-10"></div>
+          <div className="flex flex-col md:flex-row gap-5 md:gap-10">
+            {/* Vertical Bars */}
+            <div className="flex items-center">
+              <div className="relative flex items-center w-3 h-20 bg-app-bg rounded-full">
+                <div
+                  className="absolute bottom-0 bg-app-hunger w-3 rounded-full"
+                  style={{ height: "80%" }}
+                ></div>
+              </div>
+              <div className="flex items-center justify-center h-20 ml-2">
+                <img src={hunger} alt="Hunger" className="w-6 h-6" />
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className="relative flex items-center w-3 h-20 bg-app-bg rounded-full">
+                <div
+                  className="absolute bottom-0 bg-app-thirst w-3 rounded-full"
+                  style={{ height: "60%" }}
+                ></div>
+              </div>
+              <div className="flex items-center justify-center h-20 ml-2">
+                <img src={thirst} alt="Thirst" className="w-6 h-6" />
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className="relative flex items-center w-3 h-20 bg-app-bg rounded-full">
+                <div
+                  className="absolute bottom-0 bg-app-text w-3 rounded-full"
+                  style={{ height: "40%" }}
+                ></div>
+              </div>
+              <div className="flex items-center justify-center h-20 ml-2">
+                <img src={stamina} alt="Stamina" className="w-6 h-6" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
