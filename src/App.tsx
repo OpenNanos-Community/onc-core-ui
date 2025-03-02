000d3b3b96ac09da845596549c709529ad54e786
Certurix <46199479+Certurix@users.noreply.github.com>
2025-03-02 22:42:00 +0100

    style: Tweaking style of the UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Base from '@/modules/base/Base';
 
 function App() {
   return (
-    <div className="hud-container">
-      <div className="flex flex-col md:flex-row px-5 md:px-10 py-5 md:py-10 text-app-text font-inter font-bold">
-        <Base />
+    <>
+      <div className="hud-container">
+        <div className="flex flex-col md:flex-row px-5 md:px-10 py-5 md:py-10 text-app-text font-inter font-bold">
+          <Base />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
