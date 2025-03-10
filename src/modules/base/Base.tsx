399fcb40f6bc82ba9d77cfec8ce5b349dbcc6176
Certurix <46199479+Certurix@users.noreply.github.com>
2025-03-11 00:04:38 +0100

    refactor: Base component layout for improved structure and readability

--- src/modules/base/Base.tsx
+++ src/modules/base/Base.tsx
@@ -6,9 +6,9 @@ import Name from './ui/Name';
 import Bars from './ui/Bars';
 import Ammo from './ui/Ammo';
 
-
 const Base: React.FC = () => {
-    return (
+  return (
+    <div className="flex flex-col md:flex-row px-5 md:px-10 py-5 md:py-10 text-app-text font-inter font-bold hud-container">
       <div className="flex flex-col md:flex-row justify-between items-end md:gap-10">
         <div className="flex flex-col-reverse">
           <div className="flex flex-col-reverse gap-2">
@@ -21,7 +21,8 @@ const Base: React.FC = () => {
         <Bars />
         <Ammo />
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default Base;
