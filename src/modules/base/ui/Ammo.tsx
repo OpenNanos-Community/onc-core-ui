6e22fad75036c5fcabaec6c6780b77a6ea3d77a8
Paul. <59295623+trust-paulfr@users.noreply.github.com>
2025-03-25 15:22:41 +0100

    Using UseEffect to avoid hooking rendering on every change

--- src/modules/base/ui/Ammo.tsx
+++ src/modules/base/ui/Ammo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Events from '@/Events';
 import ammoIcon from '@/assets/ammo-box.svg';
 
@@ -6,12 +6,12 @@ const Ammo: React.FC = () => {
   const [currentAmmo, setCurrentAmmo] = useState(0);
   const [maxAmmo, setMaxAmmo] = useState(0);
 
-  const handleUpdateAmmo = (current: number, max: number) => {
-    setCurrentAmmo(current);
-    setMaxAmmo(max);
-  };
-
-  Events.Subscribe('ONC::UpdateAmmo', handleUpdateAmmo);
+  useEffect(() => {
+    Events.Subscribe('ONC::UpdateAmmo', (current: number, max: number) => {
+      setCurrentAmmo(current);
+      setMaxAmmo(max);
+    });
+  }, []);
 
   return (
     <div className="fixed top-4 right-4 bg-app-bg text-app-text px-4 py-2 rounded-lg shadow-lg flex items-center">
--- src/modules/base/ui/Name.tsx
+++ src/modules/base/ui/Name.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import user from '@/assets/user.svg';
 import Events from '@/Events';
 
 const Name: React.FC = () => {
-  const [name, setName] = React.useState("");
-  Events.Subscribe("ONC::UpdateName", (name: string) => {
-    setName(name);
-  });
+  const [name, setName] = useState("");
+  
+  useEffect(() => {
+    Events.Subscribe("ONC::UpdateName", (name: string) => {
+      setName(name);
+    });
+  }, []);
+
   return (
     <div className="flex flex-col justify-evenly w-full md:w-60 shrink-0">
       <div className="flex items-center bg-app-bg p-1 rounded-full mb-2">
